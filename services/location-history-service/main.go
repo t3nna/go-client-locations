2c84db2cfe00e984b33fae600e2ec905e63216f1
Ivan Melnyk <t3nna1001@gmail.com>
2025-08-21 17:43:34 +0200

    feat: add new location history service and proto definition

--- services/location-history-service/main.go
+++ services/location-history-service/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var GrpcAddr = ":9092"
 
 func main() {
-	log.Println("Hello from location history")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", GrpcAddr)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	//svc := NewService()
+	//// starting the grpcServer
+	//grpcServer := grpcserver.NewServer()
+	//NewGrpcHandler(grpcServer, svc)
+
+	log.Println("Starting gRPC server Driver service on port ", lis.Addr().String())
+
+	go func() {
+		//if err := grpcServer.Serve(lis); err != nil {
+		//	log.Printf("failed to serve: %v", err)
+		//	cancel()
+		//}
+	}()
+
+	// wait for the shutdown signal
+	<-ctx.Done()
+
+	log.Println("Shutting down the server...")
+	//grpcServer.GracefulStop()
+
 }
---
+++ services/location-history-service/service.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"go-clinet-locations/shared/types"
+	"sync"
+	"time"
+)
+
+type LocationRecord struct {
+	Coordinate *types.Coordinate
+	Timestamp  time.Time
+}
+type UserLocationHistoryService struct {
+	history map[string][]*LocationRecord
+	mu      sync.RWMutex
+}
+
+func NewService() *UserLocationHistoryService {
+	now := time.Now()
+	return &UserLocationHistoryService{
+		history: map[string][]*LocationRecord{
+			"user1": []*LocationRecord{
+				{
+					Coordinate: &types.Coordinate{
+						Latitude:  51.11822470712269,
+						Longitude: 16.990711729269563,
+					},
+					Timestamp: now.Add(-2 * time.Hour * 24),
+				},
+				{
+					Coordinate: &types.Coordinate{
+						Latitude:  51.11822470712269,
+						Longitude: 16.990711729269563,
+					},
+					Timestamp: now.Add(-1 * time.Hour),
+				},
+				{
+					Coordinate: &types.Coordinate{
+						Latitude:  51.11822470712269,
+						Longitude: 16.990711729269563,
+					},
+					Timestamp: now.Add(-30 * time.Minute),
+				},
+			},
+		},
+	}
+}
+
+func (s *UserLocationHistoryService) RegisterLocation(userId string, coords types.Coordinate, timestamp time.Time) ([]*LocationRecord, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	user, ok := s.history[userId]
+	if !ok {
+		s.history[userId] = []*LocationRecord{
+			{
+				Coordinate: &coords,
+				Timestamp:  timestamp,
+			},
+		}
+		return s.history[userId], nil
+	}
+
+	user = append(user, &LocationRecord{
+		Coordinate: &coords,
+		Timestamp:  timestamp,
+	},
+	)
+
+	return user, nil
+}
